fca4e356c9007892391d80e0ef74cbddaf145cc4
Marlon O'Neil <hello@marlononeil.com>
2017-08-10 14:36:26 -0400

    defined method with arguments

--- exercise1.rb
+++ exercise1.rb
@@ -1,4 +1,8 @@
 #Define a method that accepts two arguments representing the day of the month and the month of the year
 
-def date_method
+def date_method(day, month)
+
+
+  #returns the zodiac sign for that date.
+
 end
